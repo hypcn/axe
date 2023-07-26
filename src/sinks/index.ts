f34dd2e5724a9ad480030c7e130d5c1aa472088b
Ed Johnson <ed@neon.fish>
2023-07-26 12:49:14 +0100

    Export ObservableSink

--- src/sinks/index.ts
+++ src/sinks/index.ts
@@ -2,5 +2,6 @@
 export * from "./console-sink";
 export * from "./file-sink";
 export * from "./hypertable-sink";
+export * from "./observable-sink";
 export * from "./webhook-sink";
 export * from "./websocket-sink";
